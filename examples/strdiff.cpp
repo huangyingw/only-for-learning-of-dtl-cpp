67a3fd43507f32409692940b4eccdc71aabdf47b
huangyingw@linux_programming <huangyingw@gmail.com>
2010-10-31 03:57:27 -0400

    n

--- examples/strdiff.cpp
+++ examples/strdiff.cpp
@@ -1,4 +1,3 @@
-
 #include <dtl/dtl.hpp>
 #include "common.hpp"
 #include <iostream>
@@ -6,30 +5,16 @@
 #include <string>
 
 using namespace std;
-
 using dtl::Diff;
 
 int main(int argc, char *argv[]){
-    
-    
-    
-    typedef char   elem;
-    typedef string sequence;
-
     string A("fdadkjfeijdkfsj");
     string B("fdadkjffdkjkewfjdksj");
     
     Diff< char, string> d(A, B);
-    //d.onOnlyEditDistance();
     d.compose();
     
-    // editDistance
-    cout << "editDistance:" << d.getEditDistance() << endl;
-    
-    // Shortest Edit Script
-    cout << "UifiedFormat" << endl;
     d.composeUnifiedHunks();
     d.printUnifiedFormat();
-    
     return 0;
 }
